32c4aa4b3bc137cfe7444dce40c49933233e40f6
DhananjayKar <kardhananjay9@gmail.com>
2025-06-02 23:25:37 +0530

    Change more firebase

--- Back-End/config/firebase.js
+++ Back-End/config/firebase.js
@@ -1,8 +1,8 @@
-require("dotenv").config(); // Make sure this is at the top
+require("dotenv").config();
 
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG_JSON);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
