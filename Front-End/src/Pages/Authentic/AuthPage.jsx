c2f4d3eff494f684bad03613df230dc9105a6dbd
DhananjayKar <kardhananjay9@gmail.com>
2025-06-23 18:58:55 +0530

    change tidbit2

--- Front-End/src/Pages/Authentic/AuthPage.jsx
+++ Front-End/src/Pages/Authentic/AuthPage.jsx
@@ -22,7 +22,7 @@ const AuthPage = () => {
 
   const sendTokenToBackend = async (token) => {
     try {
-      const res = await fetch("http://localhost:5000/api/auth", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ token }),
